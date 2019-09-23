6a26a4bff89c1ba6f763216de2cfa31bc1486718
lixiaohong <lixh0870@zzstworld.com>
2019-09-23 21:35:03 +0800

    同步代码

--- libplayer/src/main/cpp/HFFmpeg.cpp
+++ libplayer/src/main/cpp/HFFmpeg.cpp
@@ -231,6 +231,11 @@ void HFFmpeg::startPlay() {
             av_packet_free(&packet);
             av_free(packet);
             packet = nullptr;
+            if((video != NULL && video->queue->getFrameQueueSize() == 0) || (audio != NULL && audio->queue->getPacketQueueSize() == 0))
+            {
+//                wlPlayStatus->exit = true;
+                break;
+            }
         }
     }
     if(mimType != NULL) {
